dafb4a6c5dbf169b7b61c9e99938fbe1634bc465
VladBI <wbayrakvlad@gmail.com>
2018-09-02 19:43:15 +0300

    tests change

--- Tests/MemCalculator_Tests.cs
+++ Tests/MemCalculator_Tests.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTtTrain;
 
 namespace Tests
 {
-    class MemCalculator_Tests
+    [TestFixture]
+    public class MemCalculator_Tests
     {
+        [Test]
+        public void Sum_ByDefault_ReturnsZero()
+        {
+            MemCalculator calc = MakeCalc();
+
+            int lastSum = calc.Sum();
+
+            Assert.AreEqual(lastSum, 0);
+        }
+
+        [Test]
+        public void Add_WhenCalled_ChangeSum()
+        {
+            MemCalculator calc = MakeCalc();
+
+            calc.Add(1);
+            int sum = calc.Sum();
+
+            Assert.AreEqual(1, sum);
+        }
+
+        private static MemCalculator MakeCalc()
+        {
+            return new MemCalculator();
+        }
+
+        [TestCase(1, 2)]
+        public void Add_AfterAdditional_ReturnsAditionalSum
+            (int first, int second)
+        {
+            MemCalculator calc = new MemCalculator();
+
+            calc.Add(first);
+            calc.Add(second);
+
+            Assert.AreEqual(calc.Sum(), 3);
+        }
     }
 }
--- UnitTtTrain/MemCalculator.cs
+++ UnitTtTrain/MemCalculator.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace UnitTtTrain
 {
-    class MemCalculator
+    public class MemCalculator
     {
+        private int sum = 0;
+        public void Add(int number)
+        {
+            sum += number;
+        }
+
+        public int Sum()
+        {
+            int temp = sum;
+            sum = 0;
+            return temp;
+        }
     }
 }
